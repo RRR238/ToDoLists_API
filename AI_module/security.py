54e3011bd21e93a63e377de7e317b2e8650c61c0
RRR238 <richardmacus0@gmail.com>
2025-02-15 16:27:15 +0100

    final adjustments

--- AI_module/security.py
+++ AI_module/security.py
@@ -21,16 +21,8 @@ def endpoint_verification(token: str = Depends(oauth2Scheme)):
 
     try:
         # Decode the JWT token
-        print(token)
-        print(Security_config.SECRET_KEY)
-        print(Security_config.ALGORITHM)
-        print(Security_config.AUDIENCE)
-        print(Security_config.ISSUER)
-        payload = jwt.decode(token, options={"verify_signature": False})
-        print("Decoded payload without verification:", payload)
         payload = jwt.PyJWT().decode(token, Security_config.SECRET_KEY, algorithms=[Security_config.ALGORITHM],
                              audience=Security_config.AUDIENCE, issuer = Security_config.ISSUER)
-        print(payload)
         return payload
     except jwt.ExpiredSignatureError:
         raise HTTPException(status_code=401, detail="Token has expired")
@@ -38,11 +30,3 @@ def endpoint_verification(token: str = Depends(oauth2Scheme)):
         raise HTTPException(status_code=401, detail="Invalid token")
     except jwt.PyJWTError:
         raise HTTPException(status_code=401, detail="Error decoding token")
-    except Exception as e:
-        print(e)
-
-try:
-    payload = jwt.PyJWT().decode("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJpbnRlcm5hbC1zZXJ2aWNlIiwiZXhwIjoxNzM5NjIzMDE1LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjcxMTgiLCJhdWQiOiJUb0RvTGlzdHNBUEkifQ.bKHi8nAc2EFEryeQR4IaqM8rOoR46n2p4vRRYW_Hv6A", Security_config.SECRET_KEY, algorithms=[Security_config.ALGORITHM],
-                             audience=Security_config.AUDIENCE, issuer = Security_config.ISSUER)
-except Exception as e:
-    print(e)
